61f3dcb0e286c6926fd287dbd1941758c41dae9c
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-09-13 00:13:35 -0600

    Added cors

--- server/main.go
+++ server/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"io/ioutil"
+	"io"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH"},
+	}))
 	router.GET("", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Everything working OK",
 		})
 	})
 	router.POST("", func(ctx *gin.Context) {
-		r, err := ioutil.ReadAll(ctx.Request.Body)
+		r, err := io.ReadAll(ctx.Request.Body)
 		if err == nil {
 			ctx.JSON(200, gin.H{
 				"message": r,
