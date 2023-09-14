4b26d87071bb02ac4e032b30ffc768b9971f2752
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-09-13 23:53:02 -0600

    Working in /posts

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,15 +1,43 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import type { test } from "../../types";
-    let data:test = {message: ""};
+	import { onMount } from 'svelte';
+	import type { post } from '../../types';
+	let data: post[] = [];
 
-    onMount(async () => {
-        const text = await fetch("http://localhost:8080/")
-        data = await text.json()
-        console.log(data)
-    })
+	onMount(async () => {
+		const response = await fetch('http://localhost:8080/posts');
+		data = await response.json();
+	});
 </script>
 
 <div class="postsListContainer">
-    <h1>{data.message}</h1>
+	<div class="postsListContent">
+        <h1>Publicaciones disponibles</h1>
+		{#if data}
+			{#each data as post}
+				<p>{post.post_content}</p>
+			{/each}
+		{/if}
+	</div>
 </div>
+
+<style>
+    .postsListContainer {
+        width: 100%;
+        height: fit-content;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .postsListContent {
+        margin: 10px;
+        height: fit-content;
+    }
+
+    .postsListContent h1:nth-child(1){
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,16 @@
 export type test = {
     message: string
 }
+
+export type post = {
+    post_id: number;
+    post_title: string;
+    post_date: number;
+    post_content: string;
+    post_tags: tag[]
+}
+
+export type tag = {
+    id:number;
+    name:string;
+}
